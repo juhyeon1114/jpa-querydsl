0c52e27695378751a3960669ecef6f3e59078467
Kai Kim <juhyeon1114@gmail.com>
2022-12-07 12:01:15 +0900

    log : 조회 api 컨트롤러

---
+++ src/main/java/study/jpaquerydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package study.jpaquerydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.jpaquerydsl.dto.MemberSearchCondition;
+import study.jpaquerydsl.dto.MemberTeamDto;
+import study.jpaquerydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
